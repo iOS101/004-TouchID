2eaef0efb5d1ecbd1764e3f7d621188d6fa90b69
liluo <i@liluo.org>
2014-09-18 15:36:50 +0800

    For IBAction.

--- 004-TouchID/ViewController.swift
+++ 004-TouchID/ViewController.swift
@@ -19,42 +19,44 @@ class ViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    if touchIDContext.canEvaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, error: &touchIDError) {
-      statusLabel.text = "正在验证 Touch ID..."
-      touchIDContext.evaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, localizedReason: "这是一个 Touch ID 的测试", reply: {
-          (success: Bool, error: NSError?) -> Void in
-            if success {
-              self.statusLabel.text = "通过验证 :)"
-            } else {
-              var errorMsg = "未通过验证 :("
-              switch error!.code {
-              case LAError.UserCancel.toRaw():
-                errorMsg = "用户取消验证"
-              case LAError.PasscodeNotSet.toRaw():
-                errorMsg = "爪机没有设置密码"
-              case LAError.SystemCancel.toRaw():
-                errorMsg = "系统中止验证"
-              case LAError.UserFallback.toRaw():
-                errorMsg = "手动输入密码去鸟"
-              case LAError.AuthenticationFailed.toRaw():
-                errorMsg = "未通过验证"
-              default:
-                errorMsg = "未知错误"
-              }
-              self.statusLabel.text = errorMsg
-            }
-        })
-    
-    } else {
-      statusLabel.text = "呃, 你的爪机似乎不支持 Touch ID..."
-    }
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
   }
 
+  @IBAction func testButton(sender: AnyObject) {
+    statusLabel.hidden = false
+    if touchIDContext.canEvaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, error: &touchIDError) {
+      statusLabel.text = "正在验证 Touch ID..."
+      touchIDContext.evaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, localizedReason: "这是一个 Touch ID 的测试", reply: {
+        (success: Bool, error: NSError?) -> Void in
+        if success {
+          self.statusLabel.text = "通过验证 :)"
+        } else {
+          var errorMsg = "未通过验证 :("
+          switch error!.code {
+          case LAError.UserCancel.toRaw():
+            errorMsg = "用户取消验证"
+          case LAError.PasscodeNotSet.toRaw():
+            errorMsg = "爪机没有设置密码"
+          case LAError.SystemCancel.toRaw():
+            errorMsg = "系统中止验证"
+          case LAError.UserFallback.toRaw():
+            errorMsg = "手动输入密码去鸟"
+          case LAError.AuthenticationFailed.toRaw():
+            errorMsg = "未通过验证"
+          default:
+            errorMsg = "未知错误"
+          }
+          self.statusLabel.text = errorMsg
+        }
+        })
+      
+    } else {
+      statusLabel.text = "呃, 你的爪机似乎不支持 Touch ID..."
+    }
+  }
 
 }
 

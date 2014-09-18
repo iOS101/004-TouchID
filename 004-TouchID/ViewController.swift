27081fc939fc2a67888461f8be07dbaffb56564e
liluo <i@liluo.org>
2014-09-18 14:42:00 +0800

    Initial Commit

---
+++ 004-TouchID/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  004-TouchID
+//
+//  Created by liluo on 9/18/14.
+//  Copyright (c) 2014 liluo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ 004-TouchIDTests/_04_TouchIDTests.swift
@@ -0,0 +1,36 @@
+//
+//  _04_TouchIDTests.swift
+//  004-TouchIDTests
+//
+//  Created by liluo on 9/18/14.
+//  Copyright (c) 2014 liluo. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class _04_TouchIDTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
